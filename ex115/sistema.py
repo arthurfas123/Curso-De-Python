c4a15d92811bd101a8562d2c3a90fe2d5a3c360d
arthurfas123 <arthurfas123@gmail.com>
2022-03-11 16:55:13 -0300

    ok

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -1,15 +1,28 @@
 from ex115.lib.interface import *
+from ex115.lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
     resp = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
     if resp == 1:
-        cabeçalho('Opção 1')
+        # Opção de listar o conteudo de um arquivo.
+        lerArquivo(arq)
     elif resp == 2:
-        cabeçalho('Opção 2')
+        # Opção para cadastrar novo usuario.
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('NOME: '))
+        idade = leiaInt('IDADE: ')
+        cadastrar(arq, nome, idade)
     elif resp == 3:
+        # Opção para sair do menu.
         cabeçalho('Saindo do sistema... Até logo!')
         break
     else:
+        # Digitou uma opção errada no menu.
         print('\033[31mErro: Digite uma opção valida!\033[m')
     sleep(2)
